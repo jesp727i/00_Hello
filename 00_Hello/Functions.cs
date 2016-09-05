ab2da734c7aefd961986d09d3b102920660cb680
jespe <jespe@DESKTOP-RE9P93D>
2016-09-05 10:42:08 +0200

    Add project files.

---
+++ 00_Hello/Functions.cs
@@ -0,0 +1,15 @@
+﻿namespace _00_Hello
+{
+    public class Functions
+    {
+        public static string Hello()
+        {
+            return "hello";
+        }
+
+        public static string Hello(string navn)
+        {
+            return "hello" + navn;
+        }
+    }
+}
---
+++ 00_Hello/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _00_Hello
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        string i = "hello";
+        [TestMethod]
+        public void TestHello()
+        {
+            Assert.AreEqual(i, Functions.Hello());
+        }
+        [TestMethod]
+        public void TestHelloFred()
+        {
+            Assert.AreEqual(i, ", Fred", Functions.Hello(", Fred"));
+        }
+    }
+}
